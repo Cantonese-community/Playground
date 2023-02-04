8b6ae8286308805aeb4c88a1808d50b4d7345074
StepfenShawn <m18824909883@163.com>
2023-02-04 18:30:57 +0800

    Show the version!!!

--- src/app.go
+++ src/app.go
@@ -4,12 +4,29 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"os/exec"
+	"bytes"
 )
+
+func get_cantonese_version() string {
+	cmd := exec.Command("cantonese", "-v")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	cmd.Run()
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	if len(errStr) != 0 {
+		return errStr
+	} else {
+		return outStr
+	}
+}
 
 func editor_page(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
+		version := get_cantonese_version()
 		t, _ := template.ParseFiles("../template/index.html")
-		log.Println(t.Execute(w, nil))
+		log.Println(t.Execute(w, version))
 	}
 }
 
